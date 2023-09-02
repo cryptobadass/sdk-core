aef9666da40acbe851d669857f9fd96d9c3d015f
cryptobadass <cryptobadass@users.noreply.github.com>
2023-09-03 07:26:08 +0800

    update dist

--- dist/utils/sortedInsert.d.ts
+++ dist/utils/sortedInsert.d.ts
@@ -1,6 +1,1 @@
-export declare function sortedInsert<T>(
-  items: T[],
-  add: T,
-  maxSize: number,
-  comparator: (a: T, b: T) => number
-): T | null
+export declare function sortedInsert<T>(items: T[], add: T, maxSize: number, comparator: (a: T, b: T) => number): T | null;
