d6c49585c8d79d6d66e8fd243353c2c5121941d4
Lint Action <lint-action@samuelmeuli.com>
2023-09-02 23:26:52 +0000

    Fix code style issues with Prettier

--- dist/entities/nativeCurrency.d.ts
+++ dist/entities/nativeCurrency.d.ts
@@ -1,8 +1,8 @@
-import { BaseCurrency } from './baseCurrency';
+import { BaseCurrency } from './baseCurrency'
 /**
  * Represents the native currency of the chain on which it resides, e.g.
  */
 export declare abstract class NativeCurrency extends BaseCurrency {
-    readonly isNative: true;
-    readonly isToken: false;
+  readonly isNative: true
+  readonly isToken: false
 }
