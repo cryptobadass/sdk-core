4ab87396a731b6890a671777bceac8dab17e3d05
Lint Action <lint-action@samuelmeuli.com>
2023-09-04 15:18:48 +0000

    Fix code style issues with Prettier

--- dist/utils/validateAndParseAddress.d.ts
+++ dist/utils/validateAndParseAddress.d.ts
@@ -2,9 +2,9 @@
  * Validates an address and returns the parsed (checksummed) version of that address
  * @param address the unchecksummed hex address
  */
-export declare function validateAndParseAddress(address: string): string;
+export declare function validateAndParseAddress(address: string): string
 /**
  * Checks if an address is valid by checking 0x prefix, length === 42 and hex encoding.
  * @param address the unchecksummed hex address
  */
-export declare function checkValidAddress(address: string): string;
+export declare function checkValidAddress(address: string): string
